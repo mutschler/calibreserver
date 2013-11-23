eead0ec801264122020506114299d3fb50d66e67
cytec <iamcytec@googlemail.com>
2013-11-23 20:28:27 +0100

    add helper functions

--- cps/config.py
+++ cps/config.py
@@ -42,6 +42,7 @@ def check_setting_int(config, cfg_name, item_name, def_val):
 CheckSection('General')
 DB_ROOT = check_setting_str(CFG, 'General', 'DB_ROOT', os.path.join(os.getcwd(), "Calibre Library"))
 TEMPLATEDIR = check_setting_str(CFG, 'General', 'TEMPLATEDIR', os.path.join(os.getcwd(), "views"))
+MAIN_DIR = check_setting_str(CFG, 'General', 'MAIN_DIR', os.getcwd())
 PORT = check_setting_int(CFG, 'General', 'PORT', 8083)
 NEWEST_BOOKS = check_setting_str(CFG, 'General', 'NEWEST_BOOKS', 60)
 RANDOM_BOOKS = check_setting_int(CFG, 'General', 'RANDOM_BOOKS', 6)
@@ -49,13 +50,20 @@ ALL_BOOKS = check_setting_str(CFG, 'General', 'ALL_BOOKS', 100)
 USER = check_setting_str(CFG, 'General', 'USER', "admin")
 PASS = check_setting_str(CFG, 'General', 'PASS', "calibreserver")
 WEB_ADMIN = bool(check_setting_int(CFG, 'General', 'WEB_ADMIN', 1))
-DEVELOPMENT = bool(check_setting_int(CFG, 'General', 'DEVELOPMENT', 0))
+DEVELOPMENT = bool(check_setting_int(CFG, 'General', 'DEVELOPMENT', 1))
+MAIL_SERVER = check_setting_str(check_setting_int(CFG, 'General', 'MAIL_SERVER', "mail.example.com"))
+MAIL_LOGIN = check_setting_str(check_setting_int(CFG, 'General', 'MAIL_LOGIN', "mail@example.com"))
+MAIL_PASSWORD = check_setting_str(check_setting_int(CFG, 'General', 'MAIL_PASSWORD', "mypassword"))
+MAIL_PORT = bool(check_setting_int(CFG, 'General', 'MAIL_PORT', 25))
+MAIL_FROM = check_setting_str(check_setting_int(CFG, 'General', 'MAIL_FROM', "library automailer <mail@example.com>"))
+
 
 SYS_ENCODING="UTF-8"
 
 configval={}
 configval["DB_ROOT"] = DB_ROOT
 configval["TEMPLATEDIR"] = TEMPLATEDIR
+configval["MAIN_DIR"] = MAIN_DIR
 configval["PORT"] = PORT
 configval["NEWEST_BOOKS"] = NEWEST_BOOKS
 configval["ALL_BOOKS"] = ALL_BOOKS
@@ -63,6 +71,14 @@ configval["USER"] = USER
 configval["PASS"] = PASS
 configval["WEB_ADMIN"] = WEB_ADMIN
 configval["DEVELOPMENT"] = DEVELOPMENT
+configval["MAIL_SERVER"] = MAIL_SERVER
+configval["MAIL_FROM"] = MAIL_FROM
+configval["MAIL_PORT"] = MAIL_PORT
+configval["MAIL_LOGIN"] = MAIL_LOGIN
+configval["MAIL_PASSWORD"] = MAIL_PASSWORD
+
+
+
 
 def save_config(configval):
     new_config = ConfigObj()
@@ -70,6 +86,7 @@ def save_config(configval):
     new_config['General'] = {}
     new_config['General']['DB_ROOT'] = configval["DB_ROOT"]
     new_config['General']['TEMPLATEDIR'] = configval["TEMPLATEDIR"]
+    new_config['General']['MAIN_DIR'] = configval["MAIN_DIR"]
     new_config['General']['PORT'] = configval["PORT"]
     new_config['General']['NEWEST_BOOKS'] = configval["NEWEST_BOOKS"]
     new_config['General']['ALL_BOOKS'] = configval["ALL_BOOKS"]
@@ -77,6 +94,11 @@ def save_config(configval):
     new_config['General']['PASS'] = configval["PASS"]
     new_config['General']['WEB_ADMIN'] = int(configval["WEB_ADMIN"])
     new_config['General']['DEVELOPMENT'] = int(configval["DEVELOPMENT"])
+    new_config['General']['MAIL_PORT'] = int(configval["MAIL_PORT"])
+    new_config['General']['MAIL_SERVER'] = configval["MAIL_SERVER"]
+    new_config['General']['MAIL_FROM'] = configval["MAIL_FROM"]
+    new_config['General']['MAIL_LOGIN'] = configval["MAIL_LOGIN"]
+    new_config['General']['MAIL_PASSWORD'] = configval["MAIL_PASSWORD"]
     new_config.write()
     return "Saved"
 
--- cps/helper.py
+++ cps/helper.py
@@ -1,94 +1,128 @@
-from calibre import db
-from calibre import config
+from cps import db
+from cps import config
 
+import smtplib
+import sys
 import os
+import traceback
+from StringIO import StringIO
+from email import encoders
+from email.MIMEBase import MIMEBase
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.generator import Generator
+from sqlalchemy import inspect
+import subprocess
 
-def searchAuthor(authorname):
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.authors.any(db.Authors.name.like(u'%{0}%'.format(authorname))))
-        .all()
-	)
-	return result
+def make_mobi(book_ids):
+    kindlegen = os.path.join(config.MAIN_DIR, "kindlegen")
+    counter = 1
+    for book_id in book_ids:
+        book = db.session.query(db.Books).filter(db.Books.id == book_id).first()
 
-def searchTitle(titlename):
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.title.like(u'%{0}%'.format(titlename)))
-        .all()
-	)
-	return result
+        file_path = os.path.join(config.DB_ROOT, book.path, book.data[0].name)
+        # print os.path.getsize(file_path + ".epub")
+        if os.path.exists(file_path + ".epub") and not os.path.exists(file_path + ".mobi"):
+            # print u"conversion started for %s" % book.title
+            check = subprocess.call([kindlegen, file_path + ".epub"], stdout=subprocess.PIPE)
+            if not check or check < 2:
+                book.data.append(db.Data(
+                        name=book.data[0].name,
+                        format="MOBI",
+                        book=book.id,
+                        uncompressed_size=os.path.getsize(file_path + ".mobi")
+                    ))
+                db.session.commit()
 
-def searchDescription(searchfor):
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.comments.any(db.Comments.text.like(u'%{0}%'.format(searchfor))))
-        .all()
-	)
-	return result
+                print u"[%03d/%03d] CONVERTED: %s" % (counter, len(book_ids), book.title)
+                counter= counter+1
+            else:
+                print u"[%03d/%03d] FAILED: %s" % (counter, len(book_ids), book.title)
+                counter= counter+1
 
-def searchTag(tagname):
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.tags.any(db.Tags.name.like(u'%{0}%'.format(tagname))))
-        .all()
-	)
-	return result
+        else:
+            counter= counter+1
 
-def searchSeries(seriesname):
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.series.any(db.Series.name.like(u'%{0}%'.format(seriesname))))
-        .all()
-	)
-	return result
+def send_mail(book_ids, kindle_mail):
+    '''Send email with attachments'''
 
-def authorsList():
-	result = (
-    db.session.query(db.Authors)
-        .order_by(db.Authors.sort)
-        .all()
-	)
-	return result
+    is_mobi = False
+    is_epub = False
+    # create MIME message
+    msg = MIMEMultipart()
+    msg['From'] = config.MAIL_FROM
+    msg['To'] = kindle_mail
+    #msg['Subject'] = 'Convert' if self.convert else 'Sent to Kindle'
+    text = 'This email has been automatically sent by library tool.'
+    msg.attach(MIMEText(text))
 
-def ratingsList(rating):
-	rating = rating*2
-	result = (
-    db.session.query(db.Books)
-        .filter(db.Books.ratings.any(db.Ratings.rating == rating))
-        .all()
-	)
-	return result
+    # attach files
+    for book_id in book_ids:
+        #msg.attach(self.get_attachment(file_path))
+        
+        book = db.session.query(db.Books).filter(db.Books.id == book_id).first()
+        print "Adding attachment: %s " % book.title
+        for format in book.data:
+            if format.format == "MOBI":
+                is_mobi == True
+            if format.format == "EPUB":
+                is_epub = True
 
-def bookDetails(bookid):
-	result = (
-	    db.session.query(db.Books)
-	        .filter(db.Books.id == bookid).first()
-	)
-	return result
 
-def booksList(limit):
-	result = (
-    db.session.query(db.Books)
-        .order_by(db.Books.sort)
-        .limit(limit)
-	)
-	return result
+        if is_mobi:
+            file_path = os.path.join(config.DB_ROOT, book.path, format.name + ".mobi")
+            msg.attach(get_attachment(file_path))
 
-def newestBooks(limit):
-	result = (
-    db.session.query(db.Books)
-        .order_by(db.Books.last_modified)
-        .limit(limit)
-	)
-	return result
+        if is_epub and not is_mobi:
+            file_path = os.path.join(config.DB_ROOT, book.path, format.name + ".epub")
+            msg.attach(get_attachment(file_path))
 
-def doSearch(searchfor, searchtype):
-	if (searchtype == "authors"):
-		result = searchAuthor(searchfor)
-	if (searchtype == "description"):
-		result = searchDescription(searchfor)
-	if (searchtype == "title"):
-		result = searchTitle(searchfor)
 
-	return result
+    if is_epub:
+        msg['Subject'] = 'Convert'
+    else:
+        msg['Subject'] = 'Sent to Kindle'
+    #sys.exit()
+    # convert MIME message to string
+    fp = StringIO()
+    gen = Generator(fp, mangle_from_=False)
+    gen.flatten(msg)
+    msg = fp.getvalue()
+
+    # send email
+    try:
+        mail_server = smtplib.SMTP(host=config.MAIL_SERVER,
+                                      port=config.MAIL_PORT)
+        mail_server.login(config.MAIN_LOGIN, config.MAIL_PASSWORD)
+        mail_server.sendmail(config.MAIN_LOGIN, kindle_mail, msg)
+        mail_server.close()
+    except smtplib.SMTPException:
+        traceback.print_exc()
+        message = ('Communication with your SMTP server failed. Maybe '
+                   'wrong connection details? Check exception details and '
+                   'your config file: %s' % msg["Subject"])
+        print >> sys.stderr, message
+        sys.exit(7)
+
+    print('Sent email to %s' % kindle_mail)
+
+
+def get_attachment(file_path):
+    '''Get file as MIMEBase message'''
+
+    try:
+        file_ = open(file_path, 'rb')
+        attachment = MIMEBase('application', 'octet-stream')
+        attachment.set_payload(file_.read())
+        file_.close()
+        encoders.encode_base64(attachment)
+
+        attachment.add_header('Content-Disposition', 'attachment',
+                              filename=os.path.basename(file_path))
+        return attachment
+    except IOError:
+        traceback.print_exc()
+        message = ('The requested file could not be read. Maybe wrong '
+                   'permissions?')
+        print >> sys.stderr, message
+        sys.exit(6)
